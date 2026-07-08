63df2f3a691c16de50f98a5c14cfb4d1d6baac72
sandeep <sandeepkg.tech@gmail.com>
2026-07-08 23:18:16 +0530

    routes are update for team controller.js

--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createTeam, getTeam, getMyTeams, deleteTeam } = require("../controllers/teamController");
+const { createTeam, getTeam, getMyTeams, deleteTeam, addMember, listMembers, removeMember } = require("../controllers/teamController");
 const authenticate = require("../middleware/authenticate");
 
 router.use(authenticate); // every team route requires login
@@ -9,5 +9,11 @@ router.post("/", createTeam);
 router.get("/my-teams", getMyTeams);
 router.get("/:teamId", getTeam);
 router.delete("/:teamId", deleteTeam);
+
+
+router.post("/:teamId/members", addMember);
+router.get("/:teamId/members", listMembers);
+router.delete("/:teamId/members/:userId", removeMember);
+
 
 module.exports = router;
